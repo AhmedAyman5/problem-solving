9462c78b5e15568863acdd5bc2b76de5b8832453
Ahmed Ayman <121185439+AhmedAyman5@users.noreply.github.com>
2026-03-17 05:18:34 +0200

    Add files via upload

---
+++ codeforces/icpc-assiut-sheet/sheet-01-data-types-conditions/B_basic_data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    long long y;
+    char z;
+    float w;
+    double v;
+
+    cin>>x>>y>>z>>w>>v;
+    cout<<x<<"\n"<<y<<"\n"<<z<<"\n"<<w<<"\n"<<v<<"\n";
+
+    return 0;
+}
